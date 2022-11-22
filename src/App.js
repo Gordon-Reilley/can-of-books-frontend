5f65a935d150cacde587727a31563ccfb68f2dc1
Gordon P Reilley Jr <gordie378@gmail.com>
2022-11-21 21:30:50 -0500

    Working on Lab11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -21,7 +22,10 @@ class App extends React.Component {
               element={<BestBooks />}
             >
             </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route 
+            exact path="/about"
+            element={<About />}>
+            </Route>
           </Routes>
           <Footer />
         </Router>
